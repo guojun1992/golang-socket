b8b44cc33a5ed4a127578ec9df4361cb70fc2b55
guojun1992 <guojun19920529@foxmail.com>
2014-09-29 12:24:22 +0800

    golang-socket

---
+++ golang-socket/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "fmt"
+    "net"
+)
+
+func main(){
+    tcpaddr,err := net.ResolveTCPAddr("tcp",":7777") //listen 7777
+    if err!=nil {
+        panic(err.Error())
+    }
+    listener,err := net.ListenTCP("tcp",tcpaddr)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    for{
+        conn,err := listener.Accept() //wait 
+        if err != nil {
+            continue
+        }
+        go dowork(conn)  //使用协程处理请求 高并发
+    }
+}
+
+/**
+
+处理业务	
+
+**/
+
+func dowork(conn net.Conn){
+        defer conn.Close()
+        r := make([]byte,100)
+        n,err := conn.Read(r)
+        if err != nil {
+            panic("server error5")
+        }
+        fmt.Println(string(r[:n]))
+        if err !=nil {
+           
+            panic("server error5")
+        }
+        conn.Write([]byte("i am server from xiecheng"))
+//      conn.close()
+}o.Copy(os.Stdout, rep.Body)
